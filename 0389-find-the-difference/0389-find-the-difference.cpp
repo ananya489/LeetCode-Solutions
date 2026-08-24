a013abb8d0771bd3704f99735121d8296bb77453
ananya489 <Ananyarajput697@gmail.com>
2026-08-24 10:19:27 +0530

    Time: 0 ms (100%), Space: 9.1 MB (78.72%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+       char r=0;
+       for(int i=0;i<s.length();i++){
+        r=r^s[i];
+       }
+       for(int i=0;i<t.length();i++){
+        r=r^t[i];
+       }
+       return r;
+
+    }
+};
