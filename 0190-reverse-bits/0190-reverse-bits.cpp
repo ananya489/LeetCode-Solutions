9e59c08f045965747db8e76a6e70f41af16a0e10
ananya489 <Ananyarajput697@gmail.com>
2026-09-02 11:12:20 +0530

    Time: 4 ms (28.83%), Space: 8.2 MB (73.1%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int reverseBits(int n) {
-        int x = 0;
-        for (int i = 0; i < 32; i++) {
-            x = x << 1;
-            x = x | (n & 1);
-            n = n >> 1;
+        int ans=0;
+        for(int i=0;i<32;i++){
+            int bit=n&1;
+            ans=ans<<1;
+            ans|=bit;
+            n=n>>1;
         }
-        return x;
+        return ans;
     }
 };
