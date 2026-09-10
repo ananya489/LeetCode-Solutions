5bd6f2382a0a0a6074277f2c5f212be90458d314
ananya489 <Ananyarajput697@gmail.com>
2026-09-10 12:57:57 +0530

    Time: 1035 ms (5.88%), Space: 60.8 MB (5.43%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -5,24 +5,15 @@ public:
         int m=s2.length();
         if(l>m)
         return false;
-        vector<int>s1count(26,0);
-        vector<int>window(26,0);
-        for (int i = 0; i < l; i++) {
-        s1count[s1[i] - 'a']++;
-        window[s2[i] - 'a']++;
-    }
-     if (s1count == window) {
-        return true;
-    }
-    for (int i = l; i < m; i++) {
-        char enteringChar = s2[i];          
-        char leavingChar = s2[i - l];  
-        window[enteringChar - 'a']++; 
-        window[leavingChar - 'a']--;   
-        if (s1count == window) {
-            return true; 
+        string t=s1;
+        sort(t.begin(),t.end());
+        for(int i=0;i<=m-l;i++){
+            string c_window = s2.substr(i, l);
+            sort(c_window.begin(),c_window.end());
+            if(c_window==t){
+                return true;
+            }
         }
-    }
-    return false;
+        return false;
     }
 };
