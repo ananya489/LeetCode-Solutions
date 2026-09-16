1d3da7e64b3cb20daa1a838d210ae295ff8df2ba
ananya489 <Ananyarajput697@gmail.com>
2026-09-17 00:49:48 +0530

    Time: 3 ms (32.99%), Space: 69.8 MB (5.22%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -8,9 +8,10 @@ public:
         while(r<n){
             if(nums[r]==0)
             zeroes++;
-            while(zeroes>k){
-                if(nums[l]==0)
-                zeroes--;
+            if(zeroes>k){
+                if(nums[l]==0){
+                    zeroes--;
+                }
                 l++;
             }
             if(zeroes<=k){
