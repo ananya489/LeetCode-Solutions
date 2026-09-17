3ed3a39936472fddcd68e8c6afc73cb525457349
ananya489 <Ananyarajput697@gmail.com>
2026-09-17 12:34:30 +0530

    Time: 0 ms (100%), Space: 7.9 MB (92.75%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -4,8 +4,12 @@ public:
         int n=start^goal;
         int flip=0;
         while(n>0){
-            n=n&(n-1);
-            flip++;
+            // n=n&(n-1);
+            // flip++;
+            if((n&1)==1){
+                flip++;
+            }
+            n=n>>1;
         }
         return flip;
     }
